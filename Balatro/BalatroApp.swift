bb9874a149ee0e4ffdd6f82ed8e17dd7114a66c9
wilson <wwilson94624@gmail.com>
2025-11-26 17:24:43 +0800

    Initial Commit

---
+++ Balatro/BalatroApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalatroApp.swift
+//  Balatro
+//
+//  Created by 劉耀升 on 2025/11/26.
+//
+
+import SwiftUI
+
+@main
+struct BalatroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Balatro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Balatro
+//
+//  Created by 劉耀升 on 2025/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
