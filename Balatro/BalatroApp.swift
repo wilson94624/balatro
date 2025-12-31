a141bcd7e955be26e96f63c00b597508b172aa27
wilson <wwilson94624@gmail.com>
2025-12-31 16:16:51 +0800

    feat: 限制應用程式方向為橫向模式

--- Balatro/BalatroApp.swift
+++ Balatro/BalatroApp.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 @main
 struct BalatroApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .landscape
     }
 }
